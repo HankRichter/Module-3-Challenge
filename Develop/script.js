585281418aab3a2ac5ae72fbee8ee2c6253b8207
Hank Richter <120127039+HankRichter@users.noreply.github.com>
2023-02-27 19:59:28 -0500

    Created writePassword function
    added an if statement to determine if the desired length of the password was entered correctly.

--- Develop/script.js
+++ Develop/script.js
@@ -6,6 +6,13 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+function writePassword(){
+  let password = "";
+  const passwordLength = prompt("Enter password length");
+  if(!(passwordLength>7 && passwordLength<129)){
+   alert("Password length does not meet requirements");
+   return;
+  };
 
 }
 
