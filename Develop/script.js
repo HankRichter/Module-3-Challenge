2f576625222f010caa7e4e813c4528faa5d83a63
Hank Richter <120127039+HankRichter@users.noreply.github.com>
2023-02-27 20:00:28 -0500

    Edited arrays were pulling from
    Cleaned up file

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+const passwordNumbers=[0,1,2,3,4,5,6,7,8,9];
+const passwordLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const passwordUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const passwordSymbols = ["!","\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
 
-  passwordText.value = password;
 function writePassword(){
   let password = "";
   const passwordLength = prompt("Enter password length");
